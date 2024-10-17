bcff83eff32fea8f8cd227e175de058a0b65eed8
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-10-17 11:46:31 -0700

    removed uncessary functions

--- cc-script.js
+++ cc-script.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     // select all trigger elements (for ancestries)
-    const triggers = document.querySelectorAll('.trigger');
+    //const triggers = document.querySelectorAll('.trigger');
 
-    // add event listeners to each trigger element
-    triggers.forEach(trigger => {
-        const infoBox = trigger.querySelector('.info-box'); // Select the info-box within the trigger
+    // // add event listeners to each trigger element
+    // triggers.forEach(trigger => {
+    //     const infoBox = trigger.querySelector('.info-box'); // Select the info-box within the trigger
 
-        // show the info box when hovering over the trigger
-        trigger.addEventListener('mouseover', () => {
-            infoBox.style.display = 'block';
-        });
+    //     // show the info box when hovering over the trigger
+    //     trigger.addEventListener('mouseover', () => {
+    //         infoBox.style.display = 'block';
+    //     });
 
-        // hide the info box when the mouse leaves the trigger
-        trigger.addEventListener('mouseout', () => {
-            infoBox.style.display = 'none';
-        });
+    //     // hide the info box when the mouse leaves the trigger
+    //     trigger.addEventListener('mouseout', () => {
+    //         infoBox.style.display = 'none';
+    //     });
 
         
-        trigger.addEventListener('mousemove', (event) => {
-            const mouseX = event.clientX;
-            const mouseY = event.clientY;
+    //     trigger.addEventListener('mousemove', (event) => {
+    //         const mouseX = event.clientX;
+    //         const mouseY = event.clientY;
 
             
-            infoBox.style.left = `${mouseX + 15}px`; 
-            infoBox.style.top = `${mouseY + 15}px`;
-        });
-    });
+    //         infoBox.style.left = `${mouseX + 15}px`; 
+    //         infoBox.style.top = `${mouseY + 15}px`;
+    //     });
+    // });
 
     // toggle hidden content for Human ancestry
     const revealHumanButton = document.getElementById('revealHumanButton');
